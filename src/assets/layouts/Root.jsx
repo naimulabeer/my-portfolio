84afb5ad5e39803fc22ebec3cc580a3107d5a046
naimulabeer <naimul.h.abeer@gmail.com>
2023-12-11 23:43:30 +0600

    about section added

--- src/assets/layouts/Root.jsx
+++ src/assets/layouts/Root.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner/Banner";
 import NavBar from "../../components/NavBar/NavBar";
+import About from "../pages/About";
 
 function Root() {
   return (
     <div>
       <NavBar />
       <Banner />
+      <About />
     </div>
   );
 }
--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -1,8 +1,44 @@
+import { FaCode, FaPaintBrush } from "react-icons/fa";
+
 function About() {
   return (
-    <>
-      <div>About Me</div>
-    </>
+    <div id="about" className="text-center py-12 mt-10">
+      <h1 className="text-3xl font-bold mb-6">About Me</h1>
+
+      <div className="mb-6">
+        <img
+          src="/src/assets/avatarImage.png"
+          alt="Avatar"
+          className="rounded-full w-40 h-40 mx-auto"
+        />
+      </div>
+
+      <p className="text-slate-700 max-w-xl mx-auto mb-8">
+        Recent Computer Science graduate from{" "}
+        <span className=" text-amber-500">United International University</span>{" "}
+        and web development enthusiast with a Mediator personality. Specializing
+        in the MERN stack, driven by intuitive approach. With a feeling
+        orientation, crafting harmonious digital experiences and embracing
+        flexibility in a prospecting style. Currently immersed in mastering
+        Next.js, bringing a determined spirit to shape the future of web
+        development with innovation and accuracy.
+      </p>
+
+      <div className="border bg-slate-200 lg:w-1/3 md:w-1/2 mx-auto p-8">
+        <h2 className="text-xl font-bold mb-6">SELF PROCLAMATIONS:</h2>
+
+        <div className="flex justify-around">
+          <div className="flex flex-col items-center gap-4">
+            <FaCode className="text-2xl mr-2" />
+            <p>Web Developer</p>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <FaPaintBrush className="text-2xl mr-2" />
+            <p>UX Enthusiast</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
