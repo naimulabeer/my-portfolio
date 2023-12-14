f1964d52f4771fc541501bdf9804555596f98b2f
naimulabeer <naimul.h.abeer@gmail.com>
2023-12-15 04:30:04 +0600

    faded animation on work

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,7 +1,8 @@
+// Import necessary libraries and components
 import { useState } from "react";
 import projectsData from "/projects.json";
 import Title from "../components/Global/Title";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Work() {
   const [selectedProject, setSelectedProject] = useState(projectsData[0]);
@@ -14,6 +15,7 @@ function Work() {
     <div id="work" className="mt-10">
       <Title heading="Projects" />
       <div className="flex flex-col lg:flex-row lg:px-[220px] mt-10">
+        {/* Left side tabs */}
         <div className="lg:w-1/3 mt-20 lg:-mr-20">
           <h2 className="text-2xl font-bold p-4 mb-5">My Projects</h2>
           <div className="border-t-2 border-l-2 rounded-tl-xl w-20 h-20 absolute left-45 border-[#00b7c7] lg:block hidden animate-border" />
@@ -35,60 +37,69 @@ function Work() {
         </div>
         {/* Left side content */}
         <div className="flex-1 md:p-20 px-4 py-4 space-y-10 mt-20 md:mt-0 mb-20">
-          <div className="max-w-screen-sm w-full lg:mr-20 lg:px-2">
-            <img
-              src={selectedProject.image}
-              alt={selectedProject.title}
-              className="w-full h-64 md:h-80 object-cover rounded-md shadow-md mb-4"
-            />
-            <div className="flex flex-col items-start">
-              <h1 className="lg:text-3xl text-lg md:text-2xl font-bold mb-2">
-                {selectedProject.title}
-              </h1>
-              <p className=" mb-4">{selectedProject.description}</p>
-              <div className="flex flex-wrap gap-2 py-2">
-                {selectedProject.technologies.map((tech, index) => (
-                  <div
-                    key={index}
-                    className="border border-[#00b7c7] rounded-md p-2 animate-border"
-                  >
-                    {tech}
+          <AnimatePresence>
+            <motion.div
+              key={selectedProject.id}
+              initial={{ y: 1, opacity: 0 }}
+              animate={{ y: 100, opacity: 1 }}
+              exit={{ y: 1, opacity: 0 }}
+              transition={{ type: "tween", duration: 0.2 }}
+              onTransitionEnd={{ display: "none" }}
+            >
+              <div className="max-w-screen-sm w-full lg:mr-20 lg:px-2">
+                <img
+                  src={selectedProject.image}
+                  alt={selectedProject.title}
+                  className="w-full h-64 md:h-80 object-cover rounded-md shadow-md mb-4"
+                />
+                <div className="flex flex-col items-start">
+                  <h1 className="lg:text-3xl text-lg md:text-2xl font-bold mb-2">
+                    {selectedProject.title}
+                  </h1>
+                  <p className="mb-4">{selectedProject.description}</p>
+                  <div className="flex flex-wrap gap-2 py-2">
+                    {selectedProject.technologies.map((tech, index) => (
+                      <div
+                        key={index}
+                        className="border border-[#00b7c7] rounded-md p-2 animate-border"
+                      >
+                        {tech}
+                      </div>
+                    ))}
                   </div>
-                ))}
+                  <div className="flex gap-5 mt-4">
+                    <motion.a
+                      whileHover={{
+                        scale: 1.1,
+                        textShadow: "0px 0px 8px #fafafa",
+                        boxShadow: "0px 0px 10px rgb(23, 185, 188)",
+                      }}
+                      href={selectedProject.liveLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="  font-bold px-6 py-2 bg-[#00b7c7]  transition-all duration-300"
+                    >
+                      Live
+                    </motion.a>
+                    <motion.a
+                      whileHover={{
+                        scale: 1.1,
+                        textShadow: "0px 0px 8px #fafafa",
+                        boxShadow: "0px 0px 10px rgb(23, 185, 188)",
+                      }}
+                      href={selectedProject.codeLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="font-bold px-6 py-2 bg-[#00b7c7] transition-all duration-300"
+                    >
+                      Code
+                    </motion.a>
+                  </div>
+                </div>
               </div>
-              <div className="flex gap-5 mt-4">
-                <motion.a
-                  whileHover={{
-                    scale: 1.1,
-                    textShadow: "0px 0px 8px #fafafa",
-                    boxShadow: "0px 0px 10px rgb(23, 185, 188)",
-                  }}
-                  href={selectedProject.liveLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="  font-bold px-6 py-2 bg-[#00b7c7]  transition-all duration-300"
-                >
-                  Live
-                </motion.a>
-                <motion.a
-                  whileHover={{
-                    scale: 1.1,
-                    textShadow: "0px 0px 8px #fafafa",
-                    boxShadow: "0px 0px 10px rgb(23, 185, 188)",
-                  }}
-                  href={selectedProject.codeLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="font-bold px-6 py-2 bg-[#00b7c7] transition-all duration-300"
-                >
-                  Code
-                </motion.a>
-              </div>
-            </div>
-          </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
-
-        {/* Right side tabs */}
       </div>
     </div>
   );
